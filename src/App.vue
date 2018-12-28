65684028a6959b3724a6608f5403c4a25b4181e5
kdev <kdev.software1110@gmail.com>
2018-12-27 22:45:49 -0500

    preloader section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <PreLoader />
   </div>
 </template>
 
 <script>
+import PreLoader from "@/components/PreLoader.vue"
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    PreLoader
+  }
 };
 </script>
---
+++ src/components/PreLoader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="c-preloader js-preloader">
+    <div class="c-preloader__spinner t-preloader__spinner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PreLoader",
+  props: {
+    msg: String
+  }
+};
+</script>
