def812e718f2a350ca4db8bc8c9cbe6eb3a86128
kdev <kdev.software1110@gmail.com>
2018-12-27 21:51:18 -0500

    add test section

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,158 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="c-main-container js-main-container">
+      <!-- ################################ -->
+      <!-- ############ HEADER ############ -->
+      <!-- ################################ -->
+      <section class="o-section o-section--header t-section t-section--header">
+        <div class="c-header">
+          <div class="o-section__header-bg t-section__header"></div>
+          <div class="o-section__content-bg t-section__content"></div>
+
+          <div class="o-container">
+            <div class="o-section__container">
+              <header class="o-section__header c-header__header t-section__header">
+                <div class="c-header__inner-header">
+                  <div class="c-header__avatar">
+                    <div class="a-header c-avatar">
+                      <img class="c-avatar__img" src="assets/images/header/avatar.jpg" alt>
+                    </div>
+                    <!-- /c-avatar -->
+                  </div>
+                  <!-- /c-header-avatar -->
+                </div>
+                <!-- /c-header__inner-header -->
+              </header>
+              <!-- /o-section__header -->
+              <div class="o-section__content c-header__content t-section__content">
+                <div class="c-header__inner-content">
+                  <div class="c-header__top">
+                    <div class="c-header__brand">
+                      <div class="c-brand">
+                        <h1 class="c-brand__title t-title">
+                          <span class="c-brand__sizer">
+                            <span class="a-header c-brand__first-word t-title__first-word">Desmond</span>
+                            <span
+                              class="a-header c-brand__second-word t-title__second-word"
+                            >Rambowski</span>
+                          </span>
+                        </h1>
+                        <h2 class="a-header c-brand__sub-title t-sub-title">
+                          <span class="c-brand__sizer">Web Developer &amp; Front-end Expert</span>
+                        </h2>
+                      </div>
+                      <!-- /c-brand -->
+                    </div>
+                    <!-- /c-header__brand -->
+                    <ul class="c-header__social-buttons c-social-buttons">
+                      <li class="a-header">
+                        <a
+                          href="https://www.facebook.com/RuvenThemes/"
+                          target="_blank"
+                          class="c-social-button t-social-button"
+                        >
+                          <i class="fa fa-lg fa-facebook"></i>
+                        </a>
+                      </li>
+                      <li class="a-header">
+                        <a
+                          href="https://dribbble.com/"
+                          target="_blank"
+                          class="c-social-button t-social-button"
+                        >
+                          <i class="fa fa-lg fa-dribbble"></i>
+                        </a>
+                      </li>
+                      <li class="a-header">
+                        <a
+                          href="https://twitter.com/_Ruven"
+                          target="_blank"
+                          class="c-social-button t-social-button"
+                        >
+                          <i class="fa fa-lg fa-twitter"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <!-- /c-header__social-buttons -->
+                  </div>
+                  <!-- /c-header__top -->
+                  <div class="c-header__contact">
+                    <hr class="a-header c-header__contact-divider">
+
+                    <div class="o-grid">
+                      <div class="o-grid__col-md-3 o-grid__col-sm-6">
+                        <div class="a-header o-content">
+                          <div class="o-content__body">
+                            <h4>Location</h4>
+                            <address>Portland, OR</address>
+                          </div>
+                        </div>
+                        <!-- /o-content -->
+                      </div>
+                      <!-- /o-grid__col -->
+                      <div class="o-grid__col-md-3 o-grid__col-sm-6">
+                        <div class="a-header o-content">
+                          <div class="o-content__body">
+                            <h4>Phone</h4>
+                            <p>534.456.886</p>
+                          </div>
+                        </div>
+                        <!-- /o-content -->
+                      </div>
+                      <!-- /o-grid__col -->
+                      <div class="o-grid__col-md-3 o-grid__col-sm-6">
+                        <div class="a-header o-content">
+                          <div class="o-content__body">
+                            <a href="#" target="_blank" class="t-link-container">
+                              <h4>Web</h4>
+                              <p>
+                                <span class="t-link-container__item--blended">ruventhemes.com</span>
+                              </p>
+                            </a>
+                            <!-- /o-link-container -->
+                          </div>
+                        </div>
+                        <!-- /o-content -->
+                      </div>
+                      <!-- /o-grid__col -->
+                      <div class="o-grid__col-md-3 o-grid__col-sm-6">
+                        <div class="a-header o-content">
+                          <div class="o-content__body">
+                            <a href="#" target="_blank" class="t-link-container">
+                              <h4>Email</h4>
+                              <p>
+                                <span class="t-link-container__item--blended">hello@example.com</span>
+                              </p>
+                            </a>
+                            <!-- /o-link-container -->
+                          </div>
+                        </div>
+                        <!-- /o-content -->
+                      </div>
+                      <!-- /o-grid__col -->
+                    </div>
+                    <!-- /o-grid -->
+                  </div>
+                  <!-- /c-header__contact -->
+                </div>
+                <!-- /c-header__inner-content -->
+              </div>
+              <!-- /o-section__content -->
+            </div>
+            <!-- /o-section__container -->
+          </div>
+          <!-- /o-container -->
+        </div>
+        <!-- /c-header -->
+      </section>
+      <!-- /o-section -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
