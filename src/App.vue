34c8b42d8c772c9846f3b29deaced400b3fb1a9f
kdev <kdev.software1110@gmail.com>
2018-12-27 22:48:25 -0500

    Main component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <PreLoader />
+    <Main />
   </div>
 </template>
 
 <script>
 import PreLoader from "@/components/PreLoader.vue"
+import Main from "@/components/Main.vue"
 
 export default {
   name: "app",
   components: {
-    PreLoader
+    PreLoader,
+    Main
   }
 };
 </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="c-main-container  js-main-container">
+    This is main content
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    msg: String
+  }
+};
+</script>
